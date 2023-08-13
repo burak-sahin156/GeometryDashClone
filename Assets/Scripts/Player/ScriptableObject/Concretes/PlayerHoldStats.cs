0811e8a2d990eb55a052410417aa3af648989218
burak-sahin156 <burak.sahin.ayvansaray@gmail.com>
2023-08-13 11:44:00 +0300

    deleted

--- Assets/Scripts/Player/ScriptableObject/Concretes/PlayerHoldStats.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "GeometryDash/Player/Stats/HoldStats", fileName = "New Hold Stats")]
-public class PlayerHoldStats : PlayerStats
-{
-    public float Gravity;
-}
